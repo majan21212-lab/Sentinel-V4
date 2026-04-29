e2a1f4ff35e6d10b802fc6d7f7689de959d91bc7
majan21212-lab <majan21212@gmail.com>
2026-04-29 08:53:12 +0400

    Update line to escape dollar signs in main.dart

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter App',
       home: Scaffold(
-        appBar: AppBar(title: Text('My App')),  
-        body: Center(child: Text('Hello, World!')), 
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Optimizes for \$20 - \$500 balances'),
+        ),
       ),
     );
   }
 }
-
-// Your existing code above this line...
-
-// Updated section
-
-// line 503
-// Optimizes for $20 - $500 balances
-// The updated line
-Optimizes for \$20 - \$500 balances
-// ... continue with the rest of your existing code
